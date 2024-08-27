abc0428674f4c59337b57b7a8902adf9a3e33335
merienevro72 <merienevro72@yandex.ru>
2024-08-27 15:14:49 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+quantity_lessons = 12
+spend_hours = 1.5
+course_name = 'Python,'
+one_lesson_time = (spend_hours/quantity_lessons)
+print( 'курс: ',course_name, 'всего задач: ', quantity_lessons,',', 'затрачено часов:',spend_hours,',','среднее время выполнения:', one_lesson_time, 'часа.' )
